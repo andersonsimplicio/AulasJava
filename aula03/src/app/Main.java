567e1326cd9cf635c64968d5a98a4abf4ff969d6
andersonsimplicio <anderson25eu@gmail.com>
2024-08-09 12:34:41 -0300

    aula03

---
+++ aula03/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        boolean resultadoAnd = condicao1 && condicao2;  // false
+        boolean resultadoOr = condicao1 || condicao2;  // true
+        boolean resultadoNot = !condicao1;  // false
+    }
+}
